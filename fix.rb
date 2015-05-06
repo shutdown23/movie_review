48587bd7e9995c9eddb81c3da667a70105dbfcb9
shutdown23 <grant.david@live.com>
2015-05-05 20:16:57 -0400

    updated ignorefile and setup aws for image storage

---
+++ fix.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require 'rb-fsevent'
+
+fsevent = FSEvent.new
+fsevent.watch Dir.pwd do |directories|
+  puts "Detected change inside: #{directories.inspect}"
+end
+
+fsevent.run 
+
+$end
